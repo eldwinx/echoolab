768b5e9586dc294014fc5ebe9d07df2cbc3a20e7
DESKTOP-757P99K\Eldwin <eldwinx@sfu.ca>
2017-11-23 13:56:40 -0800

    test

---
+++ EchooMarketing/js/index.js
@@ -0,0 +1,39 @@
+ 
+
+
+ $(document).ready(function () {
+          // Hide the div
+          $("#boxed").hide();
+          // Show the div after 5s
+          $("#boxed").delay(800).fadeIn(100);  
+  }); 
+
+  $(document).on("scroll", function() {
+
+	if($(document).scrollTop()>100) {
+		$("header").removeClass("large").addClass("small");
+	} else {
+		$("header").removeClass("small").addClass("large");
+	}
+	
+});   
+
+
+  //scroll top
+  // When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
